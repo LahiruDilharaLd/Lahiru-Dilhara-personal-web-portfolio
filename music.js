8dff3b26b273299461dc75cab09c7c088f8288f5
Lahiru Dilhara <132160250+LahiruDilharaLd@users.noreply.github.com>
2025-03-01 11:05:50 +0530

    Update music.js

--- music.js
+++ music.js
@@ -4,7 +4,7 @@ document.addEventListener("touchstart", playMusic);
 function playMusic() {
     let audio = new Audio("Study Group.mp3");
     audio.loop = true;
-    audio.volume = 0.1; // Set volume to 30%
+    audio.volume = 0.3; // Set volume to 30%
     audio.play().catch(error => {
         console.log("Autoplay blocked: User interaction required");
     });
